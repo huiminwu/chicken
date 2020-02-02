0cc5695d046bcfa28c0d2c53dcc506a593cf8804
Ivy Mao <maoivy01@gmail.com>
2020-02-02 02:15:26 -0500

    CSS for dashboard requests

--- client/src/components/pages/Request.js
+++ client/src/components/pages/Request.js
@@ -18,7 +18,7 @@ class Request extends React.Component {
     };
   }
 
-  componentDidMount() { }
+  componentDidMount() {}
 
   toggleDropdown = (dropdown) => {
     if (this.state.showDropdown === dropdown) {
@@ -53,14 +53,14 @@ class Request extends React.Component {
     const PRODUCT_TYPES = ["flour", "sugar", "paper", "bricks", "stone", "bread"];
     const PRODUCT_DETAILS = {
       flour: [
-        { unitPrice: "$1", minUnits: "10" },
-        { unitPrice: "$2", minUnits: "30" },
+        { unitPrice: "$1.00", minUnits: "30" },
+        { unitPrice: "$2.00", minUnits: "10" },
       ],
-      sugar: [{ unitPrice: "$2", minUnits: "20" }],
-      paper: [{ unitPrice: "$3", minUnits: "30" }],
-      bricks: [{ unitPrice: "$4", minUnits: "40" }],
-      stone: [{ unitPrice: "$5", minUnits: "50" }],
-      bread: [{ unitPrice: "$6", minUnits: "60" }],
+      sugar: [{ unitPrice: "$2.00", minUnits: "20" }],
+      paper: [{ unitPrice: "$3.00", minUnits: "30" }],
+      bricks: [{ unitPrice: "$4.00", minUnits: "40" }],
+      stone: [{ unitPrice: "$5.00", minUnits: "50" }],
+      bread: [{ unitPrice: "$6.00", minUnits: "60" }],
     };
 
     const productPrices = PRODUCT_DETAILS[this.state.product];
@@ -103,52 +103,52 @@ class Request extends React.Component {
         </Helmet>
         {this.state.submitted ? (
           <div>
-            Your request has been submitted! Your dashboard will be updated when it's ready to be
+            Your request has been submitted! We'll update your dashboard when it's ready to be
             placed.
           </div>
         ) : (
-            <>
-              <div className="jeff">my name jeff</div>
-              <h1 className="page-title">submit a request</h1>
-              <div className="request-field">
-                <div className="request-field-label">Product</div>
-                <div className="dropdown-container">
-                  <div className="dropdown-first-btn" onClick={() => this.toggleDropdown("product")}>
-                    {this.state.product}
-                  </div>
-                  {this.state.showDropdown === "product" && productDropdown}
+          <>
+            <div className="jeff">my name jeff</div>
+            <h1 className="page-title">submit a request</h1>
+            <div className="request-field">
+              <div className="request-field-label">Product</div>
+              <div className="dropdown-container">
+                <div className="dropdown-first-btn" onClick={() => this.toggleDropdown("product")}>
+                  {this.state.product}
                 </div>
+                {this.state.showDropdown === "product" && productDropdown}
               </div>
-              <div className="request-field">
-                <div className="request-field-label">Price</div>
-                <div className="dropdown-container">
-                  <div className="dropdown-first-btn" onClick={() => this.toggleDropdown("price")}>
-                    {this.state.price}
-                  </div>
-                  {this.state.showDropdown === "price" && priceDropdown}
+            </div>
+            <div className="request-field">
+              <div className="request-field-label">Price</div>
+              <div className="dropdown-container">
+                <div className="dropdown-first-btn" onClick={() => this.toggleDropdown("price")}>
+                  {this.state.price}
                 </div>
+                {this.state.showDropdown === "price" && priceDropdown}
               </div>
-              <div className="request-field u-flex-alignCenter">
-                <div className="request-field-label">Quantity</div>
-                <input
-                  type="text"
-                  onChange={(event) => this.handleFieldChange("units", event.target.value)}
-                />
-              </div>
-              <div className="request-footer">
-                {this.state.error}
-                <button className="footer-btn">
-                  <a href="/dashboard">Cancel</a>
-                </button>
-                <button
-                  className="footer-btn submit-btn"
-                  onClick={(event) => this.handleSubmit(event)}
-                >
-                  Submit
+            </div>
+            <div className="request-field u-flex-alignCenter">
+              <div className="request-field-label">Quantity</div>
+              <input
+                type="text"
+                onChange={(event) => this.handleFieldChange("units", event.target.value)}
+              />
+            </div>
+            <div className="request-footer">
+              {this.state.error}
+              <button className="footer-btn">
+                <a href="/dashboard">Cancel</a>
               </button>
-              </div>
-            </>
-          )}
+              <button
+                className="footer-btn submit-btn"
+                onClick={(event) => this.handleSubmit(event)}
+              >
+                Submit
+              </button>
+            </div>
+          </>
+        )}
       </div>
     );
   }
